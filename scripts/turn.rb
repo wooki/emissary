7f567a9f1a6ce452bfe59193cfb8b55bc1dcf2c9
James Rowe <jim@jimcode.org>
2024-01-08 16:21:30 +0000

    feat: Fix script options format and add order example

--- scripts/turn.rb
+++ scripts/turn.rb
@@ -4,39 +4,34 @@ require_relative '../models/game_state'
 require_relative '../models/turn'
 
 class Turn
-
   attr_accessor :state
 
   # load game state and run turn
   def initialize(gamefile, ordersdir, reportsdir, seed)
-
     t = Emissary::Turn.new(gamefile, ordersdir, reportsdir, seed)
-
   end
-
 end
 
 # parse command line options
-options = Hash.new
-OptionParser.new do | opts |
-   opts.banner = "Usage: turn.rb [options]"
+options = {}
+OptionParser.new do |opts|
+  opts.banner = 'Usage: turn.rb [options]'
 
-   opts.on("-gGAME", "--gamefile=GAME", "File to read/write game to") do |n|
-     options[:gamefile] = n
-   end
+  opts.on('-gGAME', '--gamefile=GAME', 'File to read/write game to') do |n|
+    options[:gamefile] = n
+  end
 
-   opts.on("-oORDERSDIR", "--orders=ORDERSDIR", "folder from which to read orders") do |n|
+  opts.on('-oORDERSDIR', '--orders=ORDERSDIR', 'folder from which to read orders') do |n|
     options[:ordersdir] = n
   end
 
-  opts.on("-rREPORTSDIR", "--reports=REPORTSDIR", "folder to write report files") do |n|
+  opts.on('-rREPORTSDIR', '--reports=REPORTSDIR', 'folder to write report files') do |n|
     options[:reportsdir] = n
   end
 
-  opts.on("-sSEED", "--seed=SEED", "random seed for repeatable order run") do |n|
+  opts.on('-sSEED', '--seed=SEED', 'random seed for repeatable order run') do |n|
     options[:seed] = n.to_i
   end
-
 end.parse!
 
 ng = Turn.new options[:gamefile], options[:ordersdir], options[:reportsdir], options[:seed]
