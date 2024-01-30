36d3ab52bf7216c24c88f403b8e5bb77ec42a114
James Rowe <jim@jimcode.org>
2024-01-30 17:00:55 +0000

    feat: Update comment style, add population and bidding factors to agent quality calculation, specify requirement for best agent, fix comment typo, add conditions for agent quality based on ownership and unrest, implement random assignment of 'pips' to agent stats and enforce rule that no stat can be more than 2 higher than another

--- rules/hire_agent.rb
+++ rules/hire_agent.rb
@@ -20,12 +20,20 @@ module Emissary
 
       owned = (hex.owner == player)        
 
-      # quality of agent is based on population in hex
+      # quality of agent is based on population in hex and how much your'e bidding
+      # best agent is 9 but that requires a large sum to be spent in a large town
+      big_population = 30000
+      big_bid = 20
+      
+      
+      # if owned and unrest is high then agent is worse
+      # if owned and unrest is low then agent is better
+      # if not owned and unrest is high then agent is better
+      
+      # randomly assign "pips" to their three but rule is 
+      # that no stat can ever be more than 2 more than another
+      # so once you have 3,1,1 you can't have 4,1,1 you must have 3,2,1
 
-      # ff
-      
-      
-      
       if changes.length > 0
         msg = "Trade policy was set to #{changes.join(' and ')}."
       end
